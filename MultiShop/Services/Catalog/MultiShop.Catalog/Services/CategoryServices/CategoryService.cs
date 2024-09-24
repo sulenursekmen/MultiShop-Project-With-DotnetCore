2e3c8ae72f4401deda52cc8fad5d19b5f4e63676
sulenursekmen <sulenursekmenn@gmail.com>
2024-09-24 23:14:39 +0300

    Entities and Category Service created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _mongoCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IDatabaseSettings databaseSettings, IMapper mapper)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _mongoCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value=_mapper.Map<Category>(createCategoryDto); 
+            await _mongoCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            await _mongoCollection.DeleteOneAsync(x=>x.CategoryId==id);
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoriesAsync()
+        {
+            var values = await _mongoCollection.Find(x=>true).ToListAsync();
+            return _mapper.Map<List<ResultCategoryDto>>(values);
+        }
+
+        public async Task<GetByIdCategoryDto> GetGetByIdCategoryAsync(string id)
+        {
+            var values=await _mongoCollection.Find<Category>(x=>x.CategoryId==id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdCategoryDto>(values);
+        }
+
+        public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var values=_mapper.Map<Category>(updateCategoryDto);
+            await _mongoCollection.FindOneAndReplaceAsync(x=>x.CategoryId==updateCategoryDto.CategoryId,values);
+        }
+    }
+}
