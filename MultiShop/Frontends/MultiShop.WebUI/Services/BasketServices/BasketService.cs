903bc99b47e61c3eefc9d79237ce712279e28f87
sulenursekmen <sulenursekmenn@gmail.com>
2024-10-17 00:57:52 +0300

    mig

---
+++ MultiShop/Frontends/MultiShop.WebUI/Services/BasketServices/BasketService.cs
@@ -0,0 +1,75 @@
+﻿using MultiShop.DtoLayer.BasketDtos;
+using Newtonsoft.Json;
+
+
+namespace MultiShop.WebUI.Services.BasketServices
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task AddBasketItem(BasketItemDto basketItemDto)
+        {
+            var values = await GetBasket();
+
+            // Eğer values null ise yeni bir sepet oluşturuluyor
+            if (values == null)
+            {
+                values = new BasketTotalDto();
+                values.BasketItems = new List<BasketItemDto>();
+            }
+
+            // Eğer sepet varsa ve ürün zaten eklenmemişse sepete ekle
+            if (!values.BasketItems.Any(x => x.ProductId == basketItemDto.ProductId))
+            {
+                values.BasketItems.Add(basketItemDto);
+            }
+            else
+            {
+                // Eğer ürün zaten sepette varsa, isteğe göre miktarı artırma gibi işlemler yapılabilir.
+                // Ancak burada values sıfırlanmamalı.
+                var existingItem = values.BasketItems.FirstOrDefault(x => x.ProductId == basketItemDto.ProductId);
+                existingItem.Quantity += basketItemDto.Quantity;  // Mevcut miktara ekleyin
+            }
+
+            await SaveBasket(values);
+        }
+
+        public Task DeleteBasket(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BasketTotalDto> GetBasket()
+        {
+            //var responseMessage = await _httpClient.GetAsync("baskets");
+            //var values = await responseMessage.Content.ReadFromJsonAsync<BasketTotalDto>();
+            //return values;
+            var responseMessage = await _httpClient.GetAsync("baskets");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<BasketTotalDto>(jsonData);
+            return values;
+        }
+
+        public async Task<bool> RemoveBasketItem(string productId)
+        {
+            var values = await GetBasket();
+            var deletedItem = values.BasketItems.FirstOrDefault(x => x.ProductId == productId);
+            var result = values.BasketItems.Remove(deletedItem);
+            await SaveBasket(values);
+            return true;
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+
+            var responseMessage = await _httpClient.PostAsJsonAsync<BasketTotalDto>("baskets", basketTotalDto);
+        }
+    }
+}
+
+
+
--- MultiShop/Services/Basket/MultiShop.Basket/obj/Debug/net8.0/MultiShop.Basket.AssemblyInfo.cs
+++ MultiShop/Services/Basket/MultiShop.Basket/obj/Debug/net8.0/MultiShop.Basket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Basket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7adc81543a3c1b214cea9bb14c3a1944b1ba4f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa36c62da3b38486d56c41f06ce021b5e091ae16")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Basket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Basket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
